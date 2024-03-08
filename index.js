fcd9c1a7cbf05201aeee39ad8a4882711bb4b61e
Antooo38 <71283095+Antooo38@users.noreply.github.com>
2024-03-08 12:35:07 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+let like = 21492
+let likeAmountEl = document.getElementById("like-amount")
+
+document.getElementById("like").addEventListener("dblclick", function() {
+    like += 1
+    likeAmountEl.textContent = `${like} likes`
+})
+
+document.getElementById("icon-like").addEventListener("click", function() {
+    like += 1
+    likeAmountEl.textContent = `${like} likes`
+})
